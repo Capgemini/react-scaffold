6af6e38ed19955b60e13351571cb2af93d7a915f
Graham Taylor <gtayzlor@gmail.com>
2015-08-10 14:18:15 +0100

    #6 - add withStyles decorator

--- src/decorators/withContext.js
+++ src/decorators/withContext.js
@@ -6,12 +6,14 @@ function withContext(ComposedComponent) {
 
     static propTypes = {
       context: PropTypes.shape({
+        onInsertCss: PropTypes.func,
         onSetTitle: PropTypes.func,
         onSetMeta: PropTypes.func
       })
     };
 
     static childContextTypes = {
+      onInsertCss: PropTypes.func.isRequired,
       onSetTitle: PropTypes.func.isRequired,
       onSetMeta: PropTypes.func.isRequired
     };
@@ -19,6 +21,7 @@ function withContext(ComposedComponent) {
     getChildContext() {
       let context = this.props.context;
       return {
+        onInsertCss: context.onInsertCss || emptyFunction,
         onSetTitle: context.onSetTitle || emptyFunction,
         onSetMeta: context.onSetMeta || emptyFunction
       };
